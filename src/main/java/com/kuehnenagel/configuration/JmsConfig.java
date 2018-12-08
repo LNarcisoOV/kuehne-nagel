3b07ae6b5dfcb9e6c2f834d7815bf623613c97c4
Leonardo Narciso <lnarcisoov@gmail.com>
2018-12-08 16:09:48 -0200

    Initial class for JMS configuration.

---
+++ src/main/java/com/kuehnenagel/configuration/JmsConfig.java
@@ -0,0 +1,10 @@
+package com.kuehnenagel.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+
+@Configuration
+@EnableJms
+public class JmsConfig {
+
+}
